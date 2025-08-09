7f16c7163985964ef026aeee7c716377157d63cc
Rotimi Shaibu <shaiburotimi485@gmail.com>
2025-08-09 19:29:46 +0000

    tailwindcss and postcss creation and changes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,19 +1,6 @@
-
-const config = {
-  theme: {
-    extend: {
-      fontFamily: {
-        metal: ['var(--font-metal-mania)', 'var(--font-merienda)', 'cursive'],
-      },
-      backgroundImage: {
-        'hour-glass': "url('/public/images/hour-glass.jpg')",
-        'earth-drought':"url('/public/images/earth-drought.jpg')",
-        'nature-drought':"url('/public/images/nature-drought.jpg')",
-        'Twilight': "url('/public/images/Twilight.jpg')"
-      },
-    },
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+}
