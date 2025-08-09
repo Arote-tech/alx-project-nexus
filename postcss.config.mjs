dba796eb3e3ba2f7ff235152ad98b15e3be1123f
Rotimi Shaibu <shaiburotimi485@gmail.com>
2025-08-09 18:39:32 +0000

    postcss.config content deletion

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,20 +1,15 @@
 
 const config = {
-   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
   theme: {
     extend: {
       fontFamily: {
         metal: ['var(--font-metal-mania)', 'var(--font-merienda)', 'cursive'],
       },
       backgroundImage: {
-        'hour-glass': "url('/images/hour-glass.jpg')",
-        'earth-drought':"url('/images/earth-drought.jpg')",
-        'nature-drought':"url('/images/nature-drought.jpg')",
-        'Twilight': "url('/images/Twilight.jpg')"
+        'hour-glass': "url('/public/images/hour-glass.jpg')",
+        'earth-drought':"url('/public/images/earth-drought.jpg')",
+        'nature-drought':"url('/public/images/nature-drought.jpg')",
+        'Twilight': "url('/public/images/Twilight.jpg')"
       },
     },
   },
