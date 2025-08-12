a2b3ea53733d53f9afdc2352f4ead08e9db19481
Rotimi Shaibu <shaiburotimi485@gmail.com>
2025-08-12 14:58:34 +0000

    tailwind.config correction

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-const config = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       fontFamily: {
