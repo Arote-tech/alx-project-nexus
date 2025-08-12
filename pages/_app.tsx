c4e5bbddddff58c438c468363093a9674fc39ab8
Rotimi Shaibu <shaiburotimi485@gmail.com>
2025-08-12 11:33:13 +0000

    _app.tsx revert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import Layout from "@/components/layouts/Layout";
+import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <Layout>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </Layout>
+    </SessionProvider>
   );
 }
 
